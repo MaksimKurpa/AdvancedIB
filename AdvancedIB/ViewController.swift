2081955ab6d9dd7347c67480c1cc3eb2b67733a0
Maksim Kurpa <maksim.kurpa@apalon.com>
2018-07-26 18:41:35 +0300

    Initial Commit

---
+++ AdvancedIB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdvancedIB
+//
+//  Created by Maksim Kurpa on 7/26/18.
+//  Copyright © 2018 Maksim Kurpa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
