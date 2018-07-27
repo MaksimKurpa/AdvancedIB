d54d083a7b868e852490d225c3804dcac740e1ef
Maksim Kurpa <maksim.kurpa@apalon.com>
2018-07-27 18:47:43 +0300

    initial commit

--- AdvancedIB/ViewController.swift
+++ AdvancedIB/ViewController.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var _label: UILabel!
+    @IBOutlet weak var _squareSideConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override public var traitCollection: UITraitCollection {
+
+        let wC = UITraitCollection(horizontalSizeClass: .compact)
+        let wR = UITraitCollection(horizontalSizeClass: .regular)
+        let hC = UITraitCollection(verticalSizeClass: .compact)
+        let hR = UITraitCollection(verticalSizeClass: .regular)
+        
+            if Device.IS_4_INCHES() {
+                return UITraitCollection(traitsFrom: [wC])
+            } else if Device.IS_4_7_INCHES() {
+                return UITraitCollection(traitsFrom: [wR])
+            } else if Device.IS_5_5_INCHES() {
+                return UITraitCollection(traitsFrom: [hR])
+            } else if Device.IS_5_8_INCHES() {
+                return UITraitCollection(traitsFrom: [hC])
+            }
+            else if Device.IS_IPAD_AIR_OR_SMALL_PRO() {
+                return UITraitCollection(traitsFrom: [wC, hC])
+            } else if Device.IS_IPAD_BIG_PRO() {
+                return UITraitCollection(traitsFrom: [wR, hR])
+            }
+        
+        return super.traitCollection
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        _label.text = "Size is: \(_squareSideConstraint.constant)"
     }
 
-
+    
 }
 
---
+++ Pods/Target Support Files/Pods-AdvancedIB/Pods-AdvancedIB-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AdvancedIB : NSObject
+@end
+@implementation PodsDummy_Pods_AdvancedIB
+@end
